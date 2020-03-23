e77b25f621eab5d694387d2cf430ee9f567aa246
Klaas Pieter Annema <kp@annema.me>
2020-03-23 09:10:40 +0100

    Fail when mocking something unsupported

--- src/fakeql.test.ts
+++ src/fakeql.test.ts
@@ -1,7 +1,20 @@
+import { parse } from "graphql";
 import { fakeQL } from "./index";
 
 describe("fakeQL", () => {
-  it("works", () => {
-    fakeQL();
+  it("fails when document is not an OperationDefinition", () => {
+    const document = parse(`
+      fragment foo on Foo {
+        bar
+      }
+    `);
+
+    expect(() =>
+      fakeQL({
+        document
+      })
+    ).toThrow(
+      "FakeQL: Only definitions of kind operation can be mocked. Got definition of kind 'FragmentDefinition'"
+    );
   });
 });
--- src/fakeql.ts
+++ src/fakeql.ts
@@ -1,1 +1,15 @@
-export const fakeQL = () => {};
+import { DocumentNode } from "graphql";
+
+interface FakeQLProps {
+  document: DocumentNode;
+}
+export const fakeQL = ({ document }: FakeQLProps): Record<string, any> => {
+  const definition = document.definitions[0];
+  if (definition.kind !== "OperationDefinition") {
+    throw new Error(
+      `FakeQL: Only definitions of kind operation can be mocked. Got definition of kind '${definition.kind}'`
+    );
+  }
+
+  return {};
+};
